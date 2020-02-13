b080d2dac9f2d43dd082732dc92eb47ed2f358ae
Cody Bloemhard <cblmhrd@live.nl>
2020-02-13 10:28:47 +0100

    CodeConventions start

--- app/app/src/test/java/com/uu_uce/CodeConventions.kt
+++ app/app/src/test/java/com/uu_uce/CodeConventions.kt
@@ -1,4 +1,27 @@
 package com.uu_uce
 
+import org.junit.Test
+import org.junit.Assert.*
+
 class CodeConventions {
+    @Test
+    fun ccCheck() {
+        assertEquals(2, 1 + 1)
+    }
+
+    /*
+    Prefer functional methods over looping:
+    Prefer map,filter,forEach over using a loop.
+    */
+    @Test
+    fun ccFunctionalOverLoop(){
+        var numbers = mutableListOf(0,1,2,3)
+        //YES
+        numbers = numbers.map{n -> n + 1}.toMutableList()
+        //NO
+        for(i in 0 until numbers.size){
+            numbers[i]++
+        }
+        assertEquals(numbers, listOf(2,3,4,5))
+    }
 }
--- app/app/src/test/java/com/uu_uce/ExampleUnitTest.kt
+++ app/app/src/test/java/com/uu_uce/ExampleUnitTest.kt
@@ -1,7 +1,6 @@
 package com.uu_uce
 
 import org.junit.Test
-
 import org.junit.Assert.*
 
 /**
