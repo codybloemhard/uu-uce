8d7db381a2845ec2f9d1fa88c5b10981d30baab0
Cody Bloemhard <cblmhrd@live.nl>
2020-02-25 13:42:08 +0000

    bounding boxes

--- app/app/src/main/java/com/uu_uce/shapefiles/ShapeMap.kt
+++ app/app/src/main/java/com/uu_uce/shapefiles/ShapeMap.kt
@@ -17,17 +17,32 @@ class ShapeMap{
 
 class ShapeLayer(shapeFile: SHP_File){
     private var shapes: List<ShapeZ>
+    private var bmin = mutableListOf(Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE)
+    private var bmax = mutableListOf(0.0,0.0,0.0)
 
     init{
         shapes = shapeFile.shpShapes.map{s -> ShapeZ(s)}
+        bmin = shapes.fold(bmin, {bb, shapez ->
+            bb[0] = minOf(shapez.bmin[0], bb[0])
+            bb[1] = minOf(shapez.bmin[1], bb[1])
+            bb[2] = minOf(shapez.bmin[2], bb[2])
+            bb
+        })
+        bmax = shapes.fold(bmax, {bb, shapez ->
+            bb[0] = maxOf(shapez.bmax[0], bb[0])
+            bb[1] = maxOf(shapez.bmax[1], bb[1])
+            bb[2] = maxOf(shapez.bmax[2], bb[2])
+            bb
+        })
+        Log.d("ShapeLayer", "($bmin),($bmax)")
     }
 }
 
 class ShapeZ(shape: ShpShape){
     private var type: ShapeType
     private var points: List<Triple<Double,Double,Double>>
-    private var bmin = mutableListOf(0.0,0.0,0.0)
-    private var bmax = mutableListOf(0.0,0.0,0.0)
+    var bmin = mutableListOf(Double.MAX_VALUE,Double.MAX_VALUE,Double.MAX_VALUE)
+    var bmax = mutableListOf(0.0,0.0,0.0)
 
     init{
         when(shape.shapeType){
@@ -35,17 +50,24 @@ class ShapeZ(shape: ShpShape){
                 type = ShapeType.Polygon
                 val poly = (shape as ShpPolygon)
                 points = poly.points.map { point -> Triple(point[0], point[1], point[2])}
-                poly.boundingBox
-                bmin[0] = minOf(bmin[0], poly.boundingBox[0][0])
-                //bmin[1] = minOf(bmin[1], poly.boundingBox[0])
+                updateBB(poly.boundingBox)
             }
             ShpShape.Type.PolyLineZ -> {
                 type = ShapeType.Polygon
-                points = (shape as ShpPolyLine).points.map { point -> Triple(point[0], point[1], point[2])}
+                val poly = (shape as ShpPolyLine)
+                points = poly.points.map { point -> Triple(point[0], point[1], point[2])}
+                updateBB(poly.boundingBox)
             }
             ShpShape.Type.PointZ -> {
                 type = ShapeType.Point
-                points = listOf()
+                val point = (shape as ShpPoint).point
+                points = listOf(Triple(point[0], point[1], point[2]))
+                bmin[0] = point[0]
+                bmin[1] = point[1]
+                bmin[2] = point[2]
+                bmax[0] = point[0]
+                bmax[1] = point[1]
+                bmax[2] = point[2]
             }
             else -> {
                 Log.d("ShapeZ", "${shape.shapeType}")
@@ -53,6 +75,15 @@ class ShapeZ(shape: ShpShape){
                 points = listOf()
             }
         }
+    }
+
+    private fun updateBB(bb: Array<DoubleArray>){
+        bmin[0] = bb[0][0]
+        bmin[1] = bb[1][0]
+        bmin[2] = bb[2][0]
+        bmax[0] = bb[0][1]
+        bmax[1] = bb[1][1]
+        bmax[2] = bb[2][1]
     }
 }
 
