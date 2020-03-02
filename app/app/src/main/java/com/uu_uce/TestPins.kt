461dd075783e713a4b8316b82c9ec6ca6c54d90a
vGrol98 <roelvgrol@gmail.com>
2020-03-02 12:36:05 +0100

    Basic functionality for pin pop-up window

--- app/app/src/main/java/com/uu_uce/TestPins.kt
+++ app/app/src/main/java/com/uu_uce/TestPins.kt
@@ -1,10 +1,17 @@
 package com.uu_uce
 
-import android.content.Intent
 import android.os.Bundle
+
+import android.view.Gravity
+import android.view.ViewGroup
+
+import android.widget.Button
 import android.widget.PopupWindow
+import android.widget.TextView
+
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_test_pins.*
+
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class TestPins : AppCompatActivity() {
 
@@ -12,12 +19,37 @@ class TestPins : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_pins)
 
-        btnShowContent.setOnClickListener {
-            val intent = Intent(this, PopupWindow :: class.java)
-            intent.putExtra("popuptitle", "Title")
-            startActivity(intent)
-        }
+        val btnShowContent = findViewById<Button>(R.id.btnShowContent)
+        val parentLayout = findViewById<ConstraintLayout>(R.id.testPins)
+        var title = "Dit is de titel"
 
+        btnShowContent.setOnClickListener {
+            openPopupWindow(parentLayout, title, getString(R.string.sample_text))
+        }
     }
 
+    private fun openPopupWindow(parentLayout: ConstraintLayout, title: String, content: String) {
+
+        val layoutInflater = layoutInflater
+
+        // build an custom view (to be inflated on top of our current view & build it's popup window
+        val customView = layoutInflater.inflate(R.layout.popup_window, null)
+        val popupWindow = PopupWindow(customView,ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        // add the title for the popup window
+        val windowTitle = customView.findViewById<TextView>(R.id.popup_window_title)
+        windowTitle.text = title
+
+        // add the text for the Textview in the popup window
+        val windowContent = customView.findViewById<TextView>(R.id.popup_window_text)
+        windowContent.text = content
+
+        popupWindow.showAtLocation(parentLayout, Gravity.CENTER, 0, 0)
+
+        val btnClosePopupWindow = customView.findViewById<Button>(R.id.popup_window_close_button)
+
+        btnClosePopupWindow.setOnClickListener {
+            popupWindow.dismiss()
+        }
+    }
 }
