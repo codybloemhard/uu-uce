44e8a64a6f7677cc8bbd940d73f3ea0c1182a88c
YouriRombouts <youri.rombouts@gmail.com>
2020-03-10 16:49:32 +0100

    Open pop-up from pin

--- app/app/src/main/java/com/uu_uce/pins/Pin.kt
+++ app/app/src/main/java/com/uu_uce/pins/Pin.kt
@@ -1,8 +1,16 @@
 package com.uu_uce.pins
 
+import android.app.Activity
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.PopupWindow
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.uu_uce.R
 import com.uu_uce.mapOverlay.coordToScreen
 import com.uu_uce.services.UTMCoordinate
 import com.uu_uce.shapefiles.p3
@@ -29,6 +37,28 @@ class Pin(
         val location : Pair<Float, Float> = coordToScreen(coordinate, viewport, view)
         image.setBounds((location.first - pinSize/2).roundToInt(), (location.second - imageHeight).roundToInt(), (location.first + pinSize/2).roundToInt(), (location.second).roundToInt())
         image.draw(canvas)
+    }
+
+    fun openPopupWindow(parentLayout: ConstraintLayout, activity : Activity) {
+        // make sure we can access the Pin in the fragment
+
+        val layoutInflater = activity.layoutInflater
+
+        // build an custom view (to be inflated on top of our current view & build it's popup window)
+        val customView = layoutInflater.inflate(R.layout.popup_window, null)
+        val popupWindow = PopupWindow(customView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        // add the title for the popup window
+        val windowTitle = customView.findViewById<TextView>(R.id.popup_window_title)
+        windowTitle.text = title
+
+        popupWindow.showAtLocation(parentLayout, Gravity.CENTER, 0, 0)
+
+        val btnClosePopupWindow = customView.findViewById<Button>(R.id.popup_window_close_button)
+
+        btnClosePopupWindow.setOnClickListener {
+            popupWindow.dismiss()
+        }
     }
 }
 
