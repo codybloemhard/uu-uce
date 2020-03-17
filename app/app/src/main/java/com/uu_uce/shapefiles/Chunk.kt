68c2e13ef5f84a9a303b9f116d3d846a4dcbd1f5
CooleKakkerlak <32516068+CooleKakkerlak@users.noreply.github.com>
2020-03-17 22:12:49 +0100

    binaryshapereader start
    shapes are weird

--- app/app/src/main/java/com/uu_uce/shapefiles/Chunk.kt
+++ app/app/src/main/java/com/uu_uce/shapefiles/Chunk.kt
@@ -8,8 +8,9 @@ import com.uu_uce.mapOverlay.aaBoundingBoxIntersect
 typealias ChunkIndex = Triple<Int,Int,Int>
 
 class Chunk(
-    chunkIndex: ChunkIndex,
-    var shapes: List<ShapeZ>
+    var shapes: List<ShapeZ>,
+    var bmin: p3,
+    var bmax: p3
 ) {
 
     fun draw(canvas: Canvas, paint: Paint, viewport : Pair<p2,p2>, width: Int, height: Int){
--- app/app/src/main/java/com/uu_uce/shapefiles/ShapeLayer.kt
+++ app/app/src/main/java/com/uu_uce/shapefiles/ShapeLayer.kt
@@ -15,113 +15,25 @@ import kotlin.math.pow
 class ShapeLayer(path: File, private val nrOfLODs: Int){
     private var lastViewport: Pair<p2,p2> = Pair(p2Zero,p2Zero)
     private var lastZoom: Int = -1
-    private var allShapes: List<ShapeZ>
     private lateinit var zoomShapes: List<List<ShapeZ>>
-    private var chunksBackup: MutableMap<Triple<Int, Int, Int>, Chunk> = mutableMapOf()
     private val chunks: MutableMap<Triple<Int, Int, Int>, Chunk> = mutableMapOf()
 
     private val chunkLoaders: MutableList<Pair<ChunkIndex,Job>> = mutableListOf()
+    @ExperimentalUnsignedTypes
+    private val binShapeReader: BinShapeReader = BinShapeReader(path, nrOfLODs)
 
-    var bmin = p3Zero
+    var bmin: p3
         private set
-    var bmax = p3Zero
+    var bmax: p3
         private set
-    val zDens = hashMapOf<Int,Int>()
-    private val zDensSorted: List<Int>
+
 
     init{
-        val shapefile = SHP_File(null, path)
-        shapefile.read()
-
-        allShapes = shapefile.shpShapes.map{ s -> ShapeZ(s) }
-        allShapes = allShapes.sortedBy{ it.meanZ() }
-
-        allShapes.map{
-                s ->
-            val mz = s.meanZ()
-            val old = zDens[mz] ?: 0
-            val new = old + 1
-            zDens.put(mz, new)
-        }
-        zDensSorted = zDens.keys.sorted()
-
-        createBB()
-        createZoomLevels()
-
-        for(i in 0 until nrOfLODs){
-            chunksBackup[Triple(0,0,i)] = Chunk(Triple(0,0,i),zoomShapes[i])
-        }
-    }
-
-    private fun createBB(){
-        val bminmax = mergeBBs(
-            allShapes.map{ s -> s.bMin},
-            allShapes.map{ s -> s.bMax})
-        bmin = bminmax.first
-        bmax = bminmax.second
-    }
-
-    private fun createZoomLevels(){
-        val indices: MutableList<Int> = mutableListOf()
-        var nrHeights = 0
-        var curPow = log(zDensSorted.size.toDouble(), 2.0).toInt() + 1
-        var curStep = 0
-        var stepSize: Int = 1 shl curPow
-        zoomShapes = List(nrOfLODs){ i->
-            val level = (i+1).toDouble()/nrOfLODs
-            val factor = maxOf(level.pow(3), 0.1)
-            val totalHeights =
-                if(i == nrOfLODs-1) zDensSorted.size
-                else (factor*zDensSorted.size).toInt()
-
-            while(nrHeights < totalHeights){
-                val index: Int = curStep * stepSize
-                if (index >= zDensSorted.size) {
-                    curPow--
-                    stepSize = 1 shl curPow
-                    curStep = 1
-                    continue
-                }
-                if(indices.contains(index))
-                    throw Exception("uh oh")
-
-                indices.add(index)
-                nrHeights++
-                curStep+=2
-            }
-
-            val shapes: MutableList<ShapeZ> = mutableListOf()
-            indices.sort()
-            if(indices.isNotEmpty()){
-                var a = 0
-                var b = 0
-                while(a < indices.size && b < allShapes.size){
-                    val shape = allShapes[b]
-                    val z = zDensSorted[indices[a]]
-                    when {
-                        shape.meanZ() == z -> {
-                            shapes.add(allShapes[b])
-                            //val factor =(level + level.pow(3))/2
-                            //shapes.add(ShapeZ((factor), allShapes[b]))
-                            b++
-                        }
-                        shape.meanZ() < z -> b++
-                        else -> a++
-                    }
-                }
-            }
-            shapes
-        }
-        zoomShapes.map{
-                ss -> ss.map{
-                s -> s.points.size
-        }
-        }
-        val npoints = zoomShapes.fold(0){
-                r0, ss -> r0 + ss.fold(0){
-                r1, s -> r1 + s.points.size
-        }
-        }
+        val index = ChunkIndex(0,0,nrOfLODs-1)
+        val chunk = binShapeReader.getChunk(index)
+        chunks[index] = chunk
+        bmin = chunk.bmin
+        bmax = chunk.bmax
     }
 
     private fun getNewOldChunks(viewport: Pair<p2,p2>, zoomLevel: Int) : Pair<List<ChunkIndex>,List<ChunkIndex>>{
@@ -154,9 +66,8 @@ class ShapeLayer(path: File, private val nrOfLODs: Int){
 
         val routines: MutableList<Job> = mutableListOf()
         for (chunkIndex in newChunks) {
-            val c: Chunk = chunksBackup[chunkIndex] ?: continue
             val routine = GlobalScope.launch {
-                delay(500)
+                val c: Chunk = binShapeReader.getChunk(chunkIndex)
                 synchronized(chunks) {
                     chunks[chunkIndex] = c
                 }
@@ -166,7 +77,6 @@ class ShapeLayer(path: File, private val nrOfLODs: Int){
             routines.add(routine)
         }
 
-        for (chunk in oldChunks) {
         GlobalScope.launch {
             var ok = routines.isNotEmpty()
             for(routine in routines){
@@ -193,8 +103,6 @@ class ShapeLayer(path: File, private val nrOfLODs: Int){
     }
 
     fun draw(canvas: Canvas, paint: Paint, map: ShapeMap, viewport : Pair<p2,p2>, width: Int, height: Int, zoomLevel: Int){
-        if(allShapes.isEmpty()) return
-
         updateChunks(viewport, zoomLevel, map)
 
         Logger.log(LogType.Continuous, "zoom", zoomLevel.toString())
