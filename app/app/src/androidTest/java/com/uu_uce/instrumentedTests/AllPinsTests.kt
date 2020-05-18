49e4c4a26bcc1d9931f86cb7b42f047a02a076d3
YouriRombouts <youri.rombouts@gmail.com>
2020-05-18 09:48:05 +0200

    Removed debug setting

--- app/app/src/androidTest/java/com/uu_uce/instrumentedTests/AllPinsTests.kt
+++ app/app/src/androidTest/java/com/uu_uce/instrumentedTests/AllPinsTests.kt
@@ -276,6 +276,7 @@ class AllPinsTests {
             .perform(scrollTo(), click())
 
         // Check to see if popup was correct
+        sleep(500)
         onView(withId(R.id.quiz_result_text))
             .inRoot(isPlatformPopup())
             .check(matches(withText(R.string.pin_quiz_success_head)))
@@ -324,6 +325,7 @@ class AllPinsTests {
             .perform(scrollTo(), click())
 
         // Check to see if popup was correct
+        sleep(500)
         onView(withId(R.id.quiz_result_text))
             .inRoot(isPlatformPopup())
             .check(matches(withText(R.string.pin_quiz_fail_head)))
