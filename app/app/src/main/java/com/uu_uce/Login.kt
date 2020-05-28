856764d0a8196d91bc3d247e81154fbe0661abcf
YouriRombouts <youri.rombouts@gmail.com>
2020-05-28 11:22:52 +0200

    Orgselection

--- app/app/src/main/java/com/uu_uce/Login.kt
+++ app/app/src/main/java/com/uu_uce/Login.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
@@ -38,36 +39,51 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
 
-        signin_button.setOnClickListener {
-            // Get credentials
-            val username = username_field.text.toString()
-            var password = password_field.text.toString()
-            if(username.isEmpty() && password.isEmpty()){
-                Toast.makeText(this, getString(R.string.login_nocredentials_message), Toast.LENGTH_SHORT).show()
-                username_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
-                password_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
-            }
-            else if(username.isEmpty()){
-                Toast.makeText(this, getString(R.string.login_nousername_message), Toast.LENGTH_SHORT).show()
-                username_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
-            }
-            else if(password.isEmpty()){
-                Toast.makeText(this, getString(R.string.login_nopassword_message), Toast.LENGTH_SHORT).show()
-                password_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
-            }
-            else{
-                // Hash password for sending and storing
-                password = bin2hex(getHash(password))
+        //TODO: Get this information from server
+        val orgToIp = mutableMapOf<String, String>(Pair("Utrecht University", "http://131.211.31.176:8080"))
+        val orgList = orgToIp.map{ (k, _) -> k }
 
-                val loginResult = login(username, password)
-                if(loginResult){
-                    with(sharedPref.edit()) {
-                        putString("com.uu_uce.USERNAME", username)
-                        putString("com.uu_uce.PASSWORD", password)
-                        apply()
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, orgList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        orgselector.adapter = adapter
+
+        signin_button.setOnClickListener {
+            // Find Ip to send request to
+            val ip = orgToIp[orgselector.selectedItem.toString()]
+            if(ip != null){
+                // Get credentials
+                val username = username_field.text.toString()
+                var password = password_field.text.toString()
+                if(username.isEmpty() && password.isEmpty()){
+                    Toast.makeText(this, getString(R.string.login_nocredentials_message), Toast.LENGTH_SHORT).show()
+                    username_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
+                    password_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
+                }
+                else if(username.isEmpty()){
+                    Toast.makeText(this, getString(R.string.login_nousername_message), Toast.LENGTH_SHORT).show()
+                    username_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
+                }
+                else if(password.isEmpty()){
+                    Toast.makeText(this, getString(R.string.login_nopassword_message), Toast.LENGTH_SHORT).show()
+                    password_field.setHintTextColor(ResourcesCompat.getColor(resources, R.color.FusionRed, null))
+                }
+                else{
+                    // Hash password for sending and storing
+                    password = bin2hex(getHash(password))
+
+                    val loginResult = login(username, password, ip)
+                    if(loginResult){
+                        with(sharedPref.edit()) {
+                            putString("com.uu_uce.USERNAME", username)
+                            putString("com.uu_uce.PASSWORD", password)
+                            putString("com.uu_uce.SERVER_IP", ip)
+                            putString("com.uu_uce.ORGNAME", orgselector.selectedItem.toString())
+                            apply()
+                        }
+                        val intent = Intent(this, GeoMap::class.java)
+                        startActivity(intent)
                     }
-                    val intent = Intent(this, GeoMap::class.java)
-                    startActivity(intent)
                 }
             }
         }
--- app/app/src/main/java/com/uu_uce/services/CredentialServices.kt
+++ app/app/src/main/java/com/uu_uce/services/CredentialServices.kt
@@ -1,5 +1,5 @@
 package com.uu_uce.services
 
-fun login(username : String, password : String) : Boolean{
+fun login(username : String, password : String, ip : String) : Boolean{
     return true // TODO: actually attempt to log in here
 }
